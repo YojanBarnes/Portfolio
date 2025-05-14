d720a56b90e13bbe6ec2ae89bc960aabd63f99b5
yojan.b <yojanchristbarnes@gmail.com>
2025-05-14 21:31:47 +0800

    Made the page into a single page

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,5 @@
-
-//Burger Icon Toggle
 function toggle() {
     const items = document.querySelectorAll('.nav-links');
-    const navDiv = document.querySelectorAll('.nav-div');
-
     items.forEach(item => {
         item.style.display = item.style.display === 'none' ? 'flex' : 'none';
         
